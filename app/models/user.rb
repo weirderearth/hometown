e5f0870622b8133ccd751719e85b121c20b8820b
Hugo Gameiro <hmgameiro@gmail.com>
2020-12-05 23:47:59 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,7 +79,7 @@ class User < ApplicationRecord
   has_many :markers, inverse_of: :user, dependent: :destroy
 
   has_one :invite_request, class_name: 'UserInviteRequest', inverse_of: :user, dependent: :destroy
-  accepts_nested_attributes_for :invite_request, reject_if: ->(attributes) { attributes['text'].blank? }
+  accepts_nested_attributes_for :invite_request, reject_if: ->(attributes) { attributes['text'].blank? && !Setting.require_invite_text }
   validates :invite_request, presence: true, on: :create, if: -> { Setting.require_invite_text }
 
   validates :locale, inclusion: I18n.available_locales.map(&:to_s), if: :locale?
